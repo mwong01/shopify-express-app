c36c7b00f6a4e0b35b21cf5803457a56e836983a
mwong01 <mattw90@gmail.com>
2020-03-03 14:32:25 -0500

    add Shopify specific Install and Callback routes

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const request = require('request-promise');
 const apiKey = process.env.SHOPIFY_API_KEY;
 const apiSecret = process.env.SHOPIFY_API_SECRET;
 const scopes = 'read_products';
-const forwardingAddress = "https://83e324c6.ngrok.io"; // Replace this with your HTTPS Forwarding address
+const forwardingAddress = "https://83e324c6.ngrok.io"; // Replace this with your HTTPS Forwarding address fro ngrok
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -18,4 +18,73 @@ app.get('/', (req, res) => {
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
+});
+
+
+// Create Shopify-specific routes - Install route
+// The install route expects a shop URL parameter, which it uses to redirect the merchant to the Shopify app authorization prompt where they can choose to accept or reject the installation request.
+
+app.get('/shopify', (req, res) => {
+  const shop = req.query.shop;
+  if (shop) {
+    const state = nonce();
+    const redirectUri = forwardingAddress + '/shopify/callback';
+    const installUrl = 'https://' + shop +
+      '/admin/oauth/authorize?client_id=' + apiKey +
+      '&scope=' + scopes +
+      '&state=' + state +
+      '&redirect_uri=' + redirectUri;
+
+    res.cookie('state', state);
+    res.redirect(installUrl);
+  } else {
+    return res.status(400).send('Missing shop parameter. Please add ?shop=your-development-shop.myshopify.com to your request');
+  }
+});
+
+
+// Create Shopify-specific routes - Callback route
+// After a user accepts the install request, Shopify sends them to the redirect_uri that you specified in the previous step. This address needs match the URL that you entered under Whitelisted redirection URL(s) in the Partner Dashboard. The request from Shopify includes a code parameter that needs to be exchanged for a permanent access token.
+
+app.get('/shopify/callback', (req, res) => {
+  const { shop, hmac, code, state } = req.query;
+  const stateCookie = cookie.parse(req.headers.cookie).state;
+
+  if (state !== stateCookie) {
+    return res.status(403).send('Request origin cannot be verified');
+  }
+
+  if (shop && hmac && code) {
+    // DONE: Validate request is from Shopify
+    const map = Object.assign({}, req.query);
+    delete map['signature'];
+    delete map['hmac'];
+    const message = querystring.stringify(map);
+    const providedHmac = Buffer.from(hmac, 'utf-8');
+    const generatedHash = Buffer.from(
+      crypto
+        .createHmac('sha256', apiSecret)
+        .update(message)
+        .digest('hex'),
+        'utf-8'
+      );
+    let hashEquals = false;
+
+    try {
+      hashEquals = crypto.timingSafeEqual(generatedHash, providedHmac)
+    } catch (e) {
+      hashEquals = false;
+    };
+
+    if (!hashEquals) {
+      return res.status(400).send('HMAC validation failed');
+    }
+
+    res.status(200).send('HMAC validated');
+    // TODO
+    // Exchange temporary code for a permanent access token
+      // Use access token to make API call to 'shop' endpoint
+  } else {
+    res.status(400).send('Required parameters missing');
+  }
 });
