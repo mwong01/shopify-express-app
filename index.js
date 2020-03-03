b1c45adb8af0e42dcdbb49daeee2d612048b2437
mwong01 <mattw90@gmail.com>
2020-03-03 14:53:30 -0500

    update Shopify callback route

--- index.js
+++ index.js
@@ -80,11 +80,40 @@ app.get('/shopify/callback', (req, res) => {
       return res.status(400).send('HMAC validation failed');
     }
 
-    res.status(200).send('HMAC validated');
-    // TODO
-    // Exchange temporary code for a permanent access token
-      // Use access token to make API call to 'shop' endpoint
+    // DONE: Exchange temporary code for a permanent access token
+    const accessTokenRequestUrl = 'https://' + shop + '/admin/oauth/access_token';
+    const accessTokenPayload = {
+      client_id: apiKey,
+      client_secret: apiSecret,
+      code,
+    };
+
+    request.post(accessTokenRequestUrl, { json: accessTokenPayload })
+    .then((accessTokenResponse) => {
+      const accessToken = accessTokenResponse.access_token;
+      // DONE: Use access token to make API call to 'shop' endpoint
+      const shopRequestUrl = 'https://' + shop + '/admin/api/2020-01/shop.json';
+      const shopRequestHeaders = {
+        'X-Shopify-Access-Token': accessToken,
+      };
+
+      request.get(shopRequestUrl, { headers: shopRequestHeaders })
+      .then((shopResponse) => {
+        res.status(200).end(shopResponse);
+      })
+      .catch((error) => {
+        res.status(error.statusCode).send(error.error.error_description);
+      });
+    })
+    .catch((error) => {
+      res.status(error.statusCode).send(error.error.error_description);
+    });
+
   } else {
     res.status(400).send('Required parameters missing');
   }
 });
+
+// test on development store
+
+// https://83e324c6.ngrok.io/shopify?shop=matts-test-store-88888.myshopify.com
